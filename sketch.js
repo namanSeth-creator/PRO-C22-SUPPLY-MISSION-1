0d1b32bf4e9dff3a7bc7a5b8d298b5bb220eb4b9
namanSeth-creator <76241731+namanSeth-creator@users.noreply.github.com>
2021-03-25 22:35:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,34 +28,16 @@ function setup() {
     groundSprite=createSprite(width/2, height-35, width,10);
     groundSprite.shapeColor=color(255)
 
-    box1 = createSprite(500, 610, 20,100);
-	  box1.shapeColor=color("brown")
-
-	  box2 = createSprite(300,610, 20,100);
-	  box2.shapeColor=color("brown")
-
-  	box3 = createSprite(400,650, 200,20);
-	  box3.shapeColor=color("brown")
-
     engine = Engine.create();
-    World = engine.world;
+    world = engine.world;
 
-    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
     World.add(world, packageBody);
     
 
     //Create a Ground
     ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
     World.add(world, ground);
-
-    box1 = Bodies.rectangle(width/2,650,width,10,{isStatic:true});
-    World.add(world,box1);
-
-    box2 = Bodies.rectangle(width/2,650,width,10,{isStatic:true});
-    World.add(world,box2);
-
-    box3 = Bodies.rectangle(width/2,650,width,10,{isStatic:true});
-    World.add(world,box3);
 
     Engine.run(engine);
   
@@ -75,7 +57,6 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-    Matter.Body.setStatic(packageBody, fasle);
-    
-  }
+    Matter.Body.setStatic(packageBody, false);
+    }
 }
